088cc9e87b395ae0fce8947763bbe8268107d590
tmntee <107428229+tmntee@users.noreply.github.com>
2024-08-10 17:57:21 -0400

    Create home.js module to load home page through JS

---
+++ src/home.js
@@ -0,0 +1,25 @@
+let loadHomePage = function() {
+
+    const content = document.querySelector("div#content");
+    
+    document.body.removeChild(content);
+
+    const headline = document.createElement("h1");
+    headline.textContent = "Come Eat at Kirby's Kitchen!!!";
+
+    content.appendChild(headline);
+
+    const description = document.createElement("p");
+    description.textContent = "Delectable healing treats waiting for you!!";
+
+    content.appendChild(description);
+
+    const kirbyImage = document.createElement("img");
+    kirbyImage.setAttribute("src", "https://cdn.wikirby.com/thumb/7/78/KSA_Cook_Artwork.png/800px-KSA_Cook_Artwork.png");
+
+    content.appendChild(kirbyImage);
+
+    document.body.appendChild(content);
+}
+
+export { loadHomePage };
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { loadHomePage } from "./home.js";
+
+loadHomePage();
