eaccee6e16ad0ede55ddc1c048baede87832ddb3
tmntee <107428229+tmntee@users.noreply.github.com>
2024-08-10 17:15:18 -0400

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "kirby's restaurant!!!",
+            template: './src/index.html',
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
