b696b4ba5cfe64c96aa0d7b4bb5fecab56bb0c0b
tmntee <107428229+tmntee@users.noreply.github.com>
2024-08-10 18:06:34 -0400

    Create menu.js module to create menu page content

---
+++ src/menu.js
@@ -0,0 +1,27 @@
+let loadMenuPage = function() {
+    const content = document.querySelector("div#content");
+
+    document.body.removeChild(content);
+
+    const menuDiv = document.createElement("div");
+    menuDiv.setAttribute("id", "menu");
+
+    const menuItem1 = document.createElement("p");
+    menuItem1.textContent = "Car Mouth Cake";
+
+    const menuItem2 = document.createElement("p");
+    menuItem2.textContent = "Maxim Tomato";
+
+    const menuItem3 = document.createElement("p");
+    menuItem3.textContent = "Energy Drink";
+
+    const menuItem4 = document.createElement("p");
+    menuItem4.textContent = "Kirby Burger";
+
+    menuDiv.appendChild(menuItem1);
+    menuDiv.appendChild(menuItem2);
+    menuDiv.appendChild(menuItem3);
+    menuDiv.appendChild(menuItem4);
+
+    document.body.appendChild(content);
+}
