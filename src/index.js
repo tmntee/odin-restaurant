b225cda92ec07683075f391c65dd8b81cc41e5aa
tmntee <107428229+tmntee@users.noreply.github.com>
2024-08-10 18:23:06 -0400

    Add clear content function and event listeners to header buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import { loadHomePage } from "./home.js";
+import { loadMenuPage } from "./menu.js";
 
-loadHomePage();
+let clearContent = function() {
+    let contentDiv = document.querySelector("div#content");
+
+    while (contentDiv.firstChild) {
+        contentDiv.removeChild(contentDiv.lastChild);
+    }
+}
+
+const homePageButton = document.querySelector("button#home");
+console.log(homePageButton);
+homePageButton.addEventListener('click', () => { clearContent(); loadHomePage();});
+
+const menuPageButton = document.querySelector("button#menu");
+menuPageButton.addEventListener('click', () => { clearContent(); loadMenuPage();});
+
+const aboutPageButton = document.querySelector("button#about");
